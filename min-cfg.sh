71cbf65ac1389570783a46f0b27ef6f93fe9d6e3
walksanatora <walkerffo22@hotmail.com>
2020-09-18 09:26:10 -0700

    shebang and configure reauirement

--- min-cfg.sh
+++ min-cfg.sh
@@ -1,4 +1,10 @@
-
+#!/usr/bin/env bash
+if test -e configure.cfg; then
+    echo "config found loading config"
+else
+    echo "configuration is needed"
+    ./configure.sh
+fi
 mindir=$(sed -n '1p' < config.cfg)
 compile=$(sed -n '2p' < config.cfg)
 run=$(sed -n '3p' < config.cfg)
