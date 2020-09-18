8176d3d64fddf13bee11296ff24338d8305d4626
Walker Fowlkes <47339836+walksanatora@users.noreply.github.com>
2020-09-17 17:45:36 -0700

    added the more advanced gui version

---
+++ min-gui.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+tmp=$(mktemp)
+
+dialog --checklist "options:" 0 0 4 1 "6.0 version" on 2 "forcing compile" off 3 "compile server build" off 4 "compile only no run" off 2>"$tmp"
+clear
+export iknowwhatimdoing=yes #this is is only for 6.0 beta since it yells at you if you dont have it
+out=$(cat $tmp)
+echo $out | grep 1
+if [ "$?" -eq 0 ]; then
+    cd /home/$USER/Desktop/git/mindustry
+else
+    cd /home/$USER/Desktop/git/mindustry
+fi
+gitout=$(git pull)
+echo "$gitout" >> updatelogs.txt
+echo $out | grep 2
+if [ "$?" -eq 0 ]; then
+    echo $out | grep 3
+    if [ "$?" -eq 0 ]; then
+        ./gradlew server:dist
+    else
+        ./gradlew desktop:dist
+    fi
+else
+    echo "no recompile cheking if update is needed" 
+    echo $gitout | grep -v "up to date"
+    if [ "$?" -eq 0 ]; then
+      echo $out | grep 3
+        if ["$?" -eq 0 ]; then
+            ./gradlew server:dist
+        else
+            ./gradlew desktop:dist
+        fi  
+        notify-send "compile finished"
+    fi
+fi
+echo $out | grep 4
+if [ "$?" -eq 1 ]; then
+        echo $out | grep 3
+        if ["$?" -eq 0 ]; then
+            if test -e desktop/build/libs/mindustry.jar; then
+                java -jar desktop/build/libs/mindustry.jar
+            else
+                ./gradlew desktop:dist
+                java -jar desktop/build/libs/mindustry.jar
+            fi
+        else
+            if test -e server/build/libs/mindustry.jar; then
+                java -jar server/build/libs/mindustry.jar
+            else
+                ./gradlew desktop:dist
+                java -jar server/build/libs/mindustry.jar
+            fi
+        fi  
+fi
+rm $tmp
+
